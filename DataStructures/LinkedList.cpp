938cb341e08388e594ecb6891a556164f45c3546
Brandon Moretz <bmoretz@ionicsolutions.net>
2016-07-25 08:23:20 -0400

    Added missing files from Data Structures.

---
+++ DataStructures/LinkedList.cpp
@@ -0,0 +1,133 @@
+#include "stdafx.h"
+#include "LinkedList.h"
+
+using namespace std;
+
+LinkedList::LinkedList()
+{
+	head = nullptr;
+}
+
+LinkedList::~LinkedList()
+{
+	Node * current = head;
+
+	while( current != nullptr )
+	{
+		Node * tmp = current;
+
+		current = current->next;
+
+		delete tmp;
+	}
+
+	head = nullptr;
+}
+
+void LinkedList::insertBeg( int data )
+{
+	Node *newNode = new Node( data );
+
+	assert( newNode != nullptr );
+
+	if( head == nullptr ) {
+		head = newNode;
+	}
+	else {
+		newNode->next = head;
+		head = newNode;
+	}
+}
+
+void LinkedList::insertEnd( int data )
+{
+	Node *newNode = new Node( data );
+
+	if( head == nullptr ) {
+		head = newNode;
+	}
+	else {
+
+		Node * current = head;
+
+		while( current->next != nullptr )
+			current = current->next;
+
+		current->next = newNode;
+	}
+}
+
+void LinkedList::insertAt( int data, int pos )
+{
+	assert( pos >= 0 );
+
+	Node * newNode = new Node( data );
+
+	Node * current = head;
+
+	int index = 0;
+
+	while( current != nullptr )
+	{
+		index++;
+
+		if( index == ( pos - 1 ) )
+		{
+			newNode->next = current->next;
+			current->next = newNode;
+			
+			return;
+		}
+
+		current = current->next;
+	}
+}
+
+void LinkedList::removeAt( int pos )
+{
+	assert( head != nullptr );
+
+
+}
+
+string LinkedList::to_string()
+{
+	stringstream ss;
+
+	if( head == nullptr ) {
+		ss << "List is empty";
+	}
+	else {
+
+		Node * cur = head;
+
+		int index = 1;
+
+		while( cur != nullptr )
+		{
+			ss << "{i:" << index << ",v:" << cur->data << "}";
+
+			cur = cur->next;
+			index++;
+		}
+	}
+	
+	ss << endl;
+
+	return ss.str();
+}
+
+size_t LinkedList::length() const
+{
+	Node *current = head;
+
+	size_t count = 0;
+
+	while( current != nullptr )
+	{
+		current = current->next;
+		count++;
+	}
+
+	return count;
+}
---
+++ Recursion/Recursion1.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "stdafx.h"
+
+using namespace std;
+
+namespace Recursion1
+{
+	int count_down( int from );
+
+	int factorial( int number );
+	int fibonacci( int n );
+	int bunnyEars( int bunnies );
+	int bunnyEars2( int bunnies );
+	int triangle( int rows );
+	int sumDigits( int n );
+	int count7( int n );
+	int count8( int n );
+	int powerN( int base, int n );
+	int countX( string str );
+	int countHi( string str );
+	string changeXY( string str );
+	string changePi( string str );
+	string noX( string str );
+	bool array6( array<int, 10> nums, int index );
+	int array11( array<int, 10> nums, int index );
+	bool array220( int nums[], int size, int index );
+	string allStar( string str );
+	string pairStar( string str );
+	string endX( string str );
+	int countPairs( string str );
+	int countAbc( string str );
+	int count11( string str );
+	string stringClean( string str );
+	int countHi2( string str );
+	string parenBit( string str );
+	bool nestParen( string str );
+	int strCount( string str, string sub );
+	bool strCopies( string str, string sub, int n );
+	int strDist( string str, string sub );
+};

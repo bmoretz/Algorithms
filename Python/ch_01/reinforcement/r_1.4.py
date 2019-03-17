fd83f8b5b9c4bc6d8754803682aa0a698600dcb3
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-03-17 15:25:10 -0400

    Sln clean-up.
    Sln clean-up.

--- Python/ch_01/reinforcement/r_1.4.py
+++ Python/ch_01/reinforcement/r_1.4.py
@@ -3,12 +3,12 @@
 
 n = 10
 
-def sumsqs(n):
+def sum_sqs(n):
     if n <= 0:
         raise ValueError('n cannot be negative')
     else:
         return sum( [ k * k for k in range(0, n) ] )
 
 if __name__ == "__main__":
-    sumsqs(-1)
-    sumsqs(10)
+    sum_sqs(-1)
+    sum_sqs(10)
---
+++ Python/ch_03/profile.py
@@ -0,0 +1,10 @@
+from time import time
+
+start_time = time()
+
+# algo
+
+end_time = time()
+
+elapsed = end_time - start_time
+elapsed

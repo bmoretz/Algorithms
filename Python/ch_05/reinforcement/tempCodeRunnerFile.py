05a5b14fecbee15fe5695ead99f4e55ca38b18b4
Brandon Moretz <bmoretz@ionicsolutions.net>
2019-04-22 16:50:57 -0400

    ch5 reinforcement
    ch5 reinforcement

---
+++ Python/ch_05/reinforcement/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+import sys
+
+data = []
+n = 27
+
+size = 0
+
+for k in range(n):
+    a = len(data)
+    b = sys.getsizeof(data)
+
+    if b > size:
+        size = b
+        print('Length {0:3d}; Size in bytes: {1:4d}'.format(a, b))
+    
+    data.append(None)
